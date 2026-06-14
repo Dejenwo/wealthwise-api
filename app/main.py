ba8c30e3b0f7d401ef64875714b0a936164d9e18
Dejen Wogayehu <dejenwogayehu1@gmail.com>
2026-06-14 04:10:30 -0500

    Add Netlify CORS

--- app/main.py
+++ app/main.py
@@ -37,6 +37,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://localhost:5173",
+        "https://wealthwise-ati.netlify.app",
         "https://wealthwise.app",
         "https://www.wealthwise.app",
     ],
